546628f3b3fd6151db8f2885ec76b687c66cf2ed
Diana Aliyeva <daliyeva@commure.com>
2022-07-22 17:32:54 -0400

    hash

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const app = express();
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
@@ -9,6 +11,7 @@ bookRouter.route('/books')
     const response = {hello: 'This is my API'};
     res.json(response);
   });
+
 app.use('/api', bookRouter);
  
 app.get('/', (req, res)=>{
@@ -18,3 +21,23 @@ app.get('/', (req, res)=>{
 app.listen(port, ()=>{
     console.log(`Running on port ${port}`);
 });
+
+//
+const { use } = require("bcrypt/promises");
+
+app.use(express.json());
+
+// routes go here
+app.get('/', (req, res) => {
+  res.send('<h1>App Running</h1>')
+})
+
+//create a new user
+app.post('/users', async (req, res) => {
+  const name = req.body.name;
+  const password = req.body.password;
+  bcrypt.hash(password, saltRounds, async function (err, hash){
+    const newUser = await User.create ({"name": name, "password": hash});
+    res.json ({newUser})
+  })
+})
